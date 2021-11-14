c566c36840e90a907e059cddd9a6d95b7396eb46
David Ensikat <david.ensikat@energyaustralia.com.au>
2021-11-14 16:38:49 +1100

    Tmux

--- stow.sh
+++ stow.sh
@@ -1,10 +1,11 @@
         #!/bin/bash
 
-        rm -rf ~/.bashrc ~/.gitignore ~/.vimrc ~/.vim ~/.alacritty.yml ~/.config/kitty
+        rm -rf ~/.bashrc ~/.gitignore ~/.vimrc ~/.vim ~/.alacritty.yml ~/.config/kitty ~/.config/tmuxinator
         stow -vSt ~ bash
         stow -vSt ~ vim
         stow -vSt ~ alacritty
         stow -vSt ~ kitty
+        stow -vSt ~ tmuxinator
 
         rm -f ~/.gitignore
         echo "tags" > ~/.gitignore
